81a6586da35d8c24b48f23ed92e937dac5015f61
marc <mgirona29@gmail.com>
2022-03-01 11:58:29 +0000

    fix inudation functions

--- inundation_analysis.py
+++ inundation_analysis.py
@@ -8,7 +8,7 @@ working_dir = wbt.work_dir
 __all__ = ['inundation_extents',
            'inundation_extents_between']
 
-def inundation_extents(input_raster, output_polygons, threshold):
+def inundation_extents(input_raster, output_name=None, output_raster_flag=True, output_polygons_flag=False, threshold):
     """
     Calculates inundation extents based on specific threshold
     
@@ -24,27 +24,33 @@ def inundation_extents(input_raster, output_polygons, threshold):
     None
     """
 
-    output_raster = 'temp_raster_inundation_extents.tif' # string of temporary raster file
+    if output_name is None:
+        output_name = f'inundation_extents_{threshold}'
+
+    output_raster_name = f'{output_name}.tif' # string of temporary raster file
+    output_polygons_name = f'{output_name}.shp' 
     
     wbt.work_dir = working_dir
 
     wbt.conditional_evaluation(i=input_raster, # string raster input from argugment of function
-                               output=output_raster, # assigning temporary raster file string as output raster
+                               output=output_raster_name, # assigning temporary raster file string as output raster
                                statement=f"value <= {threshold}", # conditional Rust statement
                                true = 1, # assigned value of gird cell if condition is met
                                false = 'null') # assigned value of grid cell if condition is not met
     
     wbt.work_dir = working_dir
 
-    # wbt.raster_to_vector_polygons(i=output_raster, output=output_polygons) # converts temporary raster file to vector of polygon type
+    if output_polygons_flag:
+        wbt.raster_to_vector_polygons(i=output_raster_name, output=output_polygons_name) # converts temporary raster file to vector of polygon type
 
-    # wbt.work_dir = working_dir
+    wbt.work_dir = working_dir
     
-    # os.remove(os.path.join(wbt.work_dir, output_raster)) # removes temporary raster file 
+    if not(output_raster_flag):
+        os.remove(os.path.join(wbt.work_dir, output_raster_name)) # removes temporary raster file 
 
 
 
-def inundation_extents_between(input_raster, output_polygons, low_threshold, high_threshold):
+def inundation_extents_between(input_raster, output_name=None, output_raster_flag=True, output_polygons_flag=False low_threshold, high_threshold):
     """
     Calculates inundation extents between two threshold, based on low and high thresholds values.
     
@@ -60,21 +66,27 @@ def inundation_extents_between(input_raster, output_polygons, low_threshold, hig
     None
     """
 
-    output_raster = 'temp_raster_inundation_extents.tif' # string of temporary raster file
+    if output_name is None:
+        output_name = f'inundation_extents_{threshold}'
+
+    output_raster_name = f'{output_name}.tif' # string of temporary raster file
+    output_polygons_name = f'{output_name}.shp' 
     
     wbt.work_dir = working_dir
 
     wbt.conditional_evaluation(i=input_raster, # string raster input from argugment of function
-                               output=output_raster, # assigning temporary raster file string as output raster
+                               output=output_raster_name, # assigning temporary raster file string as output raster
                                statement=f"(value >= {low_threshold}) && (value < {high_threshold})", # conditional Rust statement
                                true = 1, # assigned value of gird cell if condition is met
                                false = 'null') # assigned value of grid cell if condition is not met
     
     wbt.work_dir = working_dir
 
-    # wbt.raster_to_vector_polygons(i=output_raster, output=output_polygons) # converts temporary raster file to vector of polygon type
+    if output_polygons_flag:
+        wbt.raster_to_vector_polygons(i=output_raster_name, output=output_polygons_name) # converts temporary raster file to vector of polygon type
 
-    # wbt.work_dir = working_dir
+    wbt.work_dir = working_dir
     
-    # os.remove(os.path.join(wbt.work_dir, output_raster)) # removes temporary raster file 
+    if not(output_raster_flag):
+        os.remove(os.path.join(wbt.work_dir, output_raster_name)) # removes temporary raster file 
 
